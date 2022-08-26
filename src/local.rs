a9583eb29a9d740847d8133d2ca3f6f0fe87849b
Jake O'Brien <jakeobrien44@gmail.com>
2022-08-26 10:36:20 -0700

    cauldron model

--- src/local.rs
+++ src/local.rs
@@ -12,9 +12,17 @@ fn main() {
     app.run();
 }
 
-fn spawn_local_player(mut spawn_player: EventWriter<PlayerEvent>) {
+fn spawn_local_player(mut spawn_player: EventWriter<PlayerEvent>,mut commands: Commands, _asset_server: Res<AssetServer>) {
     println!("yessir");
     info!("spawning new player");
     spawn_player.send(PlayerEvent::Spawn { id: 1 });
     spawn_player.send(PlayerEvent::SetupLocal { id: 1 });
+    commands.spawn_bundle(SceneBundle {
+        scene: _asset_server.load("models/cauldron.glb#Scene0"),
+        ..default()
+    });
+    commands.spawn_bundle(SceneBundle {
+        scene: _asset_server.load("models/cauldronmap.glb#Scene0"),
+        ..default()
+    });
 }
