b104e7ef1730fb2f448f0a1488b04956b06d65d1
Aceeri <conmcclusk@gmail.com>
2022-08-28 00:35:37 -0700

    Exclude sensors in camera raycast

--- src/player.rs
+++ src/player.rs
@@ -833,7 +833,7 @@ pub fn avoid_intersecting(
     global: Query<&GlobalTransform>,
     mut avoid: Query<(&mut Transform, &Parent, &AvoidIntersecting)>,
 ) {
-    let filter = QueryFilter::default().exclude_dynamic();
+    let filter = QueryFilter::default().exclude_dynamic().exclude_sensors();
 
     for (mut transform, parent, avoid) in &mut avoid {
         let global_transform = if let Ok(global) = global.get(parent.get()) {
