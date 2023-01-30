c0ebd99a7f892e0914808535e74f0bbce762f342
Aceeri <conmcclusk@gmail.com>
2023-01-30 14:16:10 -0800

    Example for printing out keycodes

---
+++ examples/print_keycode.rs
@@ -0,0 +1,29 @@
+use bevy::{
+    diagnostic::{DiagnosticsPlugin, FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::{CursorGrabMode, WindowPlugin},
+};
+
+use bevy_editor_pls::EditorPlugin;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: 100.0,
+                height: 100.0,
+                ..default()
+            },
+            ..default()
+        }))
+        .add_system(print_keycode)
+        .run();
+}
+
+fn print_keycode(mut previous: Local<Vec<KeyCode>>, input: Res<Input<KeyCode>>)  {
+    let pressed = input.get_pressed().cloned().collect::<Vec<_>>();
+    if *previous != pressed {
+        info!("pressed: {:?}", pressed);
+        *previous = pressed;
+    }
+}
